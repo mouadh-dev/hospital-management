88bd1f5d9287dab8a58ad9acdc5deaa911c14576
mouadh.-dev <mouadh.laa@gmail.com>
2021-03-19 13:07:20 +0100

    first commit

---
+++ app/src/main/java/com/example/stagepfe/ConnexionActivity.kt
@@ -0,0 +1,37 @@
+package com.example.stagepfe
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class ConnexionActivity : AppCompatActivity() {
+    var InscriptionButton: Button? = null
+    var ForgotPassWord: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connexion)
+
+        init()
+
+        InscriptionButton!!.setOnClickListener {
+            val intent = Intent(this, InscriptionFirstPageActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        ForgotPassWord!!.setOnClickListener{
+            var intent = Intent(this , ForgotPasswordActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun init() {
+        ForgotPassWord = findViewById<TextView>(R.id.Motdepasseoubliee)
+        InscriptionButton = findViewById<Button>(R.id.InscriptionButton)
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/stagepfe/InscriptionFirstPageActivity.kt
@@ -0,0 +1,74 @@
+package com.example.stagepfe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+
+class InscriptionFirstPageActivity : AppCompatActivity() {
+    //declaration des views
+    var ButtonReturn: Button? = null
+    var ButtonNext: Button? = null
+    var FirstName: EditText? = null
+    var LastName: EditText? = null
+    var Mail: EditText? = null
+    var Password: EditText? = null
+    var ConfirmPass: EditText? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inscription_first_page)
+        initView()
+
+
+        ButtonReturn?.setOnClickListener {
+            val intent = Intent(this, ConnexionActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+
+        ButtonNext!!.setOnClickListener {
+            if (LastName!!.text.isEmpty() || FirstName!!.text.isEmpty() || Mail!!.text.isEmpty() || Password!!.text.isEmpty() || ConfirmPass!!.text.isEmpty()) {
+                var toast = Toast.makeText(
+                    applicationContext,
+                    "le champ est obligatoire",
+                    Toast.LENGTH_SHORT
+                )
+                toast.show()
+
+            } else {
+                if (ConfirmPass!!.text.equals(Password!!.text)) {
+                    var toast = Toast.makeText(applicationContext, " Mot de passe non confirmé", Toast.LENGTH_SHORT)
+                    toast.show()
+                } else {
+
+                    var intent = Intent(this, InscriptionSecondPageActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+    }
+    private fun initView() {
+
+        ButtonReturn = findViewById<Button>(R.id.ReturnbuttonFirstPage)
+        ButtonNext = findViewById<Button>(R.id.NextButtonFirstPage)
+        LastName= findViewById(R.id.InscriptionLastNameFirstPage)
+        FirstName= findViewById(R.id.InscriptionFirstNameFirstPage)
+        Mail= findViewById(R.id.InscriptionMailFirstPage)
+        Password= findViewById(R.id.InscriptionPasswordFirstPage)
+        ConfirmPass= findViewById(R.id.InscriptionConfirmPassFirstPage)
+    }
+
+
+//    private fun validateInput():Boolean{
+//        return InscriptionNom?.text.toString().trim().equals("")
+//    }
+
+}
+
+
