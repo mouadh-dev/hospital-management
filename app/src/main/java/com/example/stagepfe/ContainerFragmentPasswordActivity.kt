f1d73d07c09031f370353b88ff59900af2ae2506
mouadh.-dev <mouadh.laa@gmail.com>
2021-03-24 19:36:35 +0100

    all button working

--- app/src/main/java/com/example/stagepfe/ContainerFragmentPasswordActivity.kt
+++ app/src/main/java/com/example/stagepfe/ContainerFragmentPasswordActivity.kt
@@ -1,10 +1,15 @@
 package com.example.stagepfe
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.ImageView
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.example.stagepfe.Fragment.ForgotPasswordFragment
+import kotlin.concurrent.fixedRateTimer
 
 class ContainerFragmentPasswordActivity : AppCompatActivity() {
     var BackIcon: ImageView? = null
@@ -13,11 +18,14 @@ class ContainerFragmentPasswordActivity : AppCompatActivity() {
         setContentView(R.layout.activity_container_fragment_password)
         init()
 
-var Password = ForgotPasswordFragment()
+        var Password = ForgotPasswordFragment()
         supportFragmentManager.beginTransaction().replace(R.id.ContainerForgotPassword,Password).commit()
 
+        }
 
-    }
+
+
+
 
     private fun init() {
 
