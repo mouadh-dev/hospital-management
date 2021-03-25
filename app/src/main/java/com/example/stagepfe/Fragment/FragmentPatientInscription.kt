9ec37b75848e59a44e94c0fe7848a3895ab3c0a4
Mohamed <mohamedrouahi4@gmail.com>
2021-03-25 23:47:53 +0100

    RadioButton

--- app/src/main/java/com/example/stagepfe/Fragment/FragmentPatientInscription.kt
+++ app/src/main/java/com/example/stagepfe/Fragment/FragmentPatientInscription.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.stagepfe.AuthenticationFragmentContainerActivity
@@ -20,6 +21,8 @@ class FragmentPatientInscription : Fragment() {
     var Condition: EditText? = null
     var FinishButton: Button? = null
     var Medicament: EditText? = null
+    var YesMedicament: RadioButton? = null
+    var NoMedicament: RadioButton? = null
 
 
     override fun onCreateView(
@@ -35,6 +38,8 @@ class FragmentPatientInscription : Fragment() {
         Condition = view.findViewById(R.id.ConditionPatient)
         ReturnButton = view.findViewById<Button>(R.id.ReturnButtonPatientInscription)
         FinishButton = view.findViewById<Button>(R.id.FinishButtonPatientInscription)
+        YesMedicament = view.findViewById<RadioButton>(R.id.YesMedicament)
+       NoMedicament = view.findViewById<RadioButton>(R.id.NoMedicament)
         Medicament = view.findViewById(R.id.Medicament)
 
         ReturnButton!!.setOnClickListener {
@@ -43,46 +48,106 @@ class FragmentPatientInscription : Fragment() {
             fragmentManager!!.beginTransaction()!!
                 .replace(R.id.ContainerFragmentLayout, ChoosePosition)!!.commit()
         }
+        fun onRadioButtonClicked(view: View) {
+            if (view is RadioButton) {
+                // Is the button now checked?
+                val checked = view.isChecked
 
-        FinishButton!!.setOnClickListener {
-        if (Condition!!.text.isEmpty() ||  Medicament!!.text.isEmpty()) {
-            var v = View.inflate(requireContext(), R.layout.fragment_dialog, null)
-            var builder = AlertDialog.Builder(requireContext())
-            builder.setView(v)
+                // Check which radio button was clicked
+                when (view.getId()) {
+                    R.id.YesMedicament ->
+                        if (checked) {
+                            Medicament!!.visibility = View.VISIBLE
+                            FinishButton!!.setOnClickListener {
+                                if (Condition!!.text.isEmpty() || Medicament!!.text.isEmpty()) {
+                                    var v = View.inflate(
+                                        requireContext(),
+                                        R.layout.fragment_dialog,
+                                        null
+                                    )
+                                    var builder = AlertDialog.Builder(requireContext())
+                                    builder.setView(v)
 
-            var dialog = builder.create()
-            dialog.show()
-            dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+                                    var dialog = builder.create()
+                                    dialog.show()
+                                    dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
 
-            dialog.findViewById<Button>(R.id.btn_confirm).setOnClickListener {
-                dialog.dismiss()
-            }
+                                    dialog.findViewById<Button>(R.id.btn_confirm)
+                                        .setOnClickListener {
+                                            dialog.dismiss()
+                                        }
 
-        } else {
-            requireActivity().run {
-                var intent = Intent(this, AuthenticationFragmentContainerActivity::class.java)
-                startActivity(intent)
-                finish()
+                                } else {
+                                    var Connexion = ConnexionFragment()
+                                    fragmentManager!!.beginTransaction()
+                                        .replace(R.id.ContainerFragmentLayout, Connexion).commit()
 
-                var congratView = View.inflate(
-                    requireContext(),
-                    R.layout.fragment_dalog_congart_frament,
-                    null
-                )
-                var builder = AlertDialog.Builder(requireContext())
-                builder.setView(congratView)
+                                        var congratView = View.inflate(
+                                            requireContext(),
+                                            R.layout.fragment_dalog_congart_frament,
+                                            null
+                                        )
+                                        var builder = AlertDialog.Builder(requireContext())
+                                        builder.setView(congratView)
 
-                var dialogcongrat = builder.create()
-                dialogcongrat.show()
-                dialogcongrat.window?.setBackgroundDrawableResource(android.R.color.transparent)
+                                        var dialogcongrat = builder.create()
+                                        dialogcongrat.show()
+                                        dialogcongrat.window?.setBackgroundDrawableResource(android.R.color.transparent)
 
-                dialogcongrat.findViewById<Button>(R.id.Congrats_Button)
-                    .setOnClickListener {
-                        dialogcongrat.dismiss()
+                                        dialogcongrat.findViewById<Button>(R.id.Congrats_Button)
+                                            .setOnClickListener {
+                                                dialogcongrat.dismiss()
+                                            }
+
+                                }
+                            } }
+                    R.id.NoMedicament ->
+                        if (checked) {
+                            Medicament!!.visibility = View.GONE
+                            FinishButton!!.setOnClickListener {
+                                if (Condition!!.text.isEmpty()) {
+                                    var v = View.inflate(requireContext(), R.layout.fragment_dialog, null)
+                                    var builder = AlertDialog.Builder(requireContext())
+                                    builder.setView(v)
+
+                                    var dialog = builder.create()
+                                    dialog.show()
+                                    dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+                                    dialog.findViewById<Button>(R.id.btn_confirm).setOnClickListener {
+                                        dialog.dismiss()
+                                    }
+
+                                } else {
+                                    var Connexion = ConnexionFragment()
+                                    fragmentManager!!.beginTransaction()
+                                        .replace(R.id.ContainerFragmentLayout, Connexion).commit()
+
+                                        var congratView = View.inflate(
+                                            requireContext(),
+                                            R.layout.fragment_dalog_congart_frament,
+                                            null
+                                        )
+                                        var builder = AlertDialog.Builder(requireContext())
+                                        builder.setView(congratView)
+
+                                        var dialogcongrat = builder.create()
+                                        dialogcongrat.show()
+                                        dialogcongrat.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+                                        dialogcongrat.findViewById<Button>(R.id.Congrats_Button)
+                                            .setOnClickListener {
+                                                dialogcongrat.dismiss()
+                                            }
+
+                                }
+                            }
+                        }
+                }
             }
         }
-        }
-    }
+
+
 
 
 }
