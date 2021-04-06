fd2f92a7ceacc0e6bad19c6bab3d5f5d5c21c8b5
mouadh.-dev <mouadh.laa@gmail.com>
2021-04-06 14:50:44 +0100

    listView

--- app/src/main/java/com/example/stagepfe/Patient/AccountPatientActivity.kt
+++ app/src/main/java/com/example/stagepfe/Patient/AccountPatientActivity.kt
@@ -1,19 +1,53 @@
 package com.example.stagepfe.Patient
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.stagepfe.Authentication.entite.UserItem
 import com.example.stagepfe.R
 
 
 class AccountPatientActivity : AppCompatActivity() {
+    var listview:ListView? = null
+    var list = mutableListOf<Model>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_patient)
+
+        listview = findViewById<ListView>(R.id.list)
+        list.add(Model("Dr Foulen Fouleni","Generaliste", R.drawable.doctor_ic))
+        list.add(Model("Dr Foulen Fouleni","Generaliste", R.drawable.doctor_ic))
+        list.add(Model("Dr Foulen Fouleni","Generaliste", R.drawable.doctor_ic))
+        list.add(Model("Dr Foulen Fouleni","Generaliste", R.drawable.doctor_ic))
+        list.add(Model("Dr Foulen Fouleni","Generaliste", R.drawable.doctor_ic))
+        list.add(Model("Dr Foulen Fouleni","Generaliste", R.drawable.doctor_ic))
+        list.add(Model("Dr Foulen Fouleni","Generaliste", R.drawable.doctor_ic))
+
+        listview!!.adapter = MyAdapter(this, R.layout.doctors_list, list)
+
+
+
+
+
+
 
         var home = PatientAccountFragment()
         supportFragmentManager.beginTransaction().replace(R.id.ContainerFragmentPatient, home)
             .commit()
 
-
     }
+//    fun fetchData():ArrayList<Model>{
+//        var list:ArrayList<Model>?= ArrayList()
+//        list!!.add(Model("Dr Foulen Fouleni","Generaliste", R.drawable.doctor_ic))
+//        list.add(Model("Dr Foulen Fouleni","Generaliste", R.drawable.doctor_ic))
+//        list.add(Model("Dr Foulen Fouleni","Generaliste", R.drawable.doctor_ic))
+//        list.add(Model("Dr Foulen Fouleni","Generaliste", R.drawable.doctor_ic))
+//        list.add(Model("Dr Foulen Fouleni","Generaliste", R.drawable.doctor_ic))
+//        list.add(Model("Dr Foulen Fouleni","Generaliste", R.drawable.doctor_ic))
+//        list.add(Model("Dr Foulen Fouleni","Generaliste", R.drawable.doctor_ic))
+//
+//        return list
+//    }
 }
---
+++ app/src/main/java/com/example/stagepfe/Patient/Model.kt
@@ -0,0 +1,4 @@
+package com.example.stagepfe.Patient
+
+class Model(val name: String, var speciality: String, var img:Int) {
+}
