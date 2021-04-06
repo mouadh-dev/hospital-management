5803097a30c12957410d308788df8ce38cedabd9
Mohamed <mohamedrouahi4@gmail.com>
2021-04-06 17:51:40 +0100

    doctor

---
+++ app/src/main/java/com/example/stagepfe/Doctor/ModelMessagePatient.kt
@@ -0,0 +1,4 @@
+package com.example.stagepfe.Doctor
+
+class ModelMessagePatient(val nom: String, var message: String, var temps: String, var pic:Int) {
+}
---
+++ app/src/main/java/com/example/stagepfe/Doctor/ModelPatientList.kt
@@ -0,0 +1,4 @@
+package com.example.stagepfe.Doctor
+
+class ModelPatientList(val nom_patient:String, var msg_patient: String, var pic_patient:Int)  {
+}
