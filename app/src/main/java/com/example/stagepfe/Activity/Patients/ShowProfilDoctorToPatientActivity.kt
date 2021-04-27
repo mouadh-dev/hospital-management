802b42421736beb099afd0c3934edb31a65db1d4
Mohamed <mohamedrouahi4@gmail.com>
2021-04-27 19:09:34 +0100

    Show info Profil Doctor

---
+++ app/src/main/java/com/example/stagepfe/Activity/Patients/ShowProfilDoctorToPatientActivity.kt
@@ -0,0 +1,11 @@
+package com.example.stagepfe.Activity.Patients
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowProfilDoctorToPatientActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_profil_doctor_to_patient)
+    }
+}
