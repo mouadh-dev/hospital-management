8889e313db7e2cbb4e83350322ef1a2f7949650c
mouadh.-dev <mouadh.laa@gmail.com>
2021-05-19 20:56:11 +0100

    Qr code work

--- app/src/main/java/com/example/stagepfe/Fragments/Patient/OrdonancePatientFragment.kt
+++ app/src/main/java/com/example/stagepfe/Fragments/Patient/OrdonancePatientFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ListView
 import android.widget.TextView
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
 import com.example.stagepfe.Adapters.Doctor.MyAdapterOrdonanceReading
 import com.example.stagepfe.Adapters.Patients.MyAdapterOrdonancePatient
@@ -88,32 +89,68 @@ class OrdonancePatientFragment : Fragment() {
             dialog.findViewById<TextView>(R.id.namePatient).setText(ordonanceList.namepatientOrdo)
             dialog.findViewById<Button>(R.id.btn_remove).setText("D'accord")
 
-            listViewOrdReading = dialog.findViewById<ListView>(R.id.List_Medicament_to_show)
-            for (medicament in ordonanceList.medicament) {
-                listMedicament.add(medicament)
+//            listViewOrdReading = dialog.findViewById<ListView>(R.id.List_Medicament_to_show)
+//            for (medicament in ordonanceList.medicament) {
+//                listMedicament.add(medicament)
+//            }
+
+//            adapterMedicament = MyAdapterOrdonanceReading(
+//                requireContext(),
+//                R.layout.ordonance_reading_doctor,
+//                listMedicament
+//            )
+//            dialog.findViewById<ListView>(R.id.List_Medicament_to_show)!!.adapter = adapterMedicament
+//            adapterMedicament!!.notifyDataSetChanged()
+////////////////////////////////////////////////////////////////////////////////////////////////////
+            // below line is for getting
+            // the windowmanager service.
+            WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+            // initializing a variable for default display.
+            Display display = manager.getDefaultDisplay();
+
+            // creating a variable for point which
+            // is to be displayed in QR Code.
+            Point point = new Point();
+            display.getSize(point);
+
+            // getting width and
+            // height of a point
+            int width = point.x;
+            int height = point.y;
+
+            // generating dimension from width and height.
+            int dimen = width < height ? width : height;
+            dimen = dimen * 3 / 4;
+
+            // setting this dimensions inside our qr code
+            // encoder to generate our qr code.
+            qrgEncoder = new QRGEncoder(dataEdt.getText().toString(), null, QRGContents.Type.TEXT, dimen);
+            try {
+                // getting our qrcode in the form of bitmap.
+                bitmap = qrgEncoder.encodeAsBitmap();
+                // the bitmap is set inside our image
+                // view using .setimagebitmap method.
+                qrCodeIV.setImageBitmap(bitmap);
+            } catch (WriterException e) {
+                // this method is called for
+                // exception handling.
+                Log.e("Tag", e.toString());
             }
 
-            adapterMedicament = MyAdapterOrdonanceReading(
-                requireContext(),
-                R.layout.ordonance_reading_doctor,
-                listMedicament
-            )
-            dialog.findViewById<ListView>(R.id.List_Medicament_to_show)!!.adapter = adapterMedicament
-            adapterMedicament!!.notifyDataSetChanged()
-
-
+///////////////////////////////////////////////////////////////////////////////////////////////////
             dialog.setOnDismissListener {
                 listMedicament.clear()
             }
 
             dialog.findViewById<Button>(R.id.btn_remove).setOnClickListener {
                 dialog.dismiss()
-
             }
+            var gson = Gson()
+            var jsonString = gson.toJson(ordonanceAdapter)
 
         }
-        var gson = Gson()
-        var jsonString = gson.toJson(listOf("mouadh","mohamed"))
+
 
 
     }
